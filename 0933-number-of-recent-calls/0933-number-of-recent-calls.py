f50757fc06c9c72ca98c5afe5af3108afb695ff7
Eifa <147934720+eifakhan@users.noreply.github.com>
2025-12-12 19:01:14 +0530

    Time: 5125 ms (6.16%), Space: 23.4 MB (8.12%) - LeetHub

---
+++ 0933-number-of-recent-calls/0933-number-of-recent-calls.py
@@ -0,0 +1,19 @@
+from collections import deque
+class RecentCounter:
+
+    def __init__(self):
+        self.q = list()
+        
+
+    def ping(self, t: int) -> int:
+        if t is None:
+            return None
+        else:
+            self.q.append(t)
+            srange = t-3000
+            erange = t
+            count = 0
+            for i in self.q:
+                if i>=srange and i<=erange:
+                    count+=1
+            return count
